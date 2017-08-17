4d719e3d12fe2c9548dc302192cf4c76c64ceb3d
seregpie@gmail.com <seregpie@gmail.com>
2017-08-17 21:53:49 +0200

    17.8.17

--- VueWordCloud.js
+++ VueWordCloud.js
@@ -1,39 +1,170 @@
 (function(Vue) {
 
+	let timeout = function(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	};
+
 	Vue.component('VueWordCloud', {
-		render(createElement) {
-			return createElement('canvas');
-		},
+		template: `
+			<div style="position: absolute; width: 100%; height: 100%;">
+				<div v-for="word in placedWords" style="position: absolute;" :style="word.style">{{ word.text }}</div>
+			</div>
+		`,
 
 		props: {
-			type: String,
-			data: Object,
-			options: Object,
+			words: {
+				type: Array,
+				default() {
+					return [];
+				},
+			},
+
+			text: {
+				type: [String, Function],
+				default: '',
+			},
+
+			fontSize: {
+				type: [Number, Function],
+				default: 16,
+			},
+
+			fontFamily: {
+				type: [String, Function],
+				default: 'serif',
+			},
+
+			fontStyle: {
+				type: [String, Function],
+				default: 'normal',
+			},
+
+			fontWeight: {
+				type: [String, Function],
+				default: 'normal',
+			},
+
+			rotate: {
+				type: [String, Function],
+				default() {
+					return function() {
+						return (~~(Math.random() * 6) - 3) * 30;
+					};
+				},
+			},
 		},
 
-		mounted() {
-			this.chart = new Chart(this.$el, {
-				type: this.type,
-				data: this.data,
-				options: this.options,
-			});
+		data() {
+			return {
+				placedWords: [],
+				size: [500, 500],
+				drawId: null,
+			};
+		},
+
+		computed: {
+			normalizedWords() {
+				return this.words.map(word => {
+					let text, fontSize, fontFamily, fontStyle, fontWeight, rotate;
+					if (word) {
+						switch (typeof word) {
+							case 'string': {
+								text = word;
+								break;
+							}
+							case 'object': {
+								if (Array.isArray(word)) {
+									([text, fontSize, fontFamily, fontStyle, fontWeight, rotate] = word);
+								} else {
+									({text, fontSize, fontFamily, fontStyle, fontWeight, rotate} = word);
+								}
+								break;
+							}
+						}
+					}
+					if (text === undefined) {
+						if (typeof this.text === 'function') {
+							text = this.text(word);
+						} else {
+							text = this.text;
+						}
+					}
+					if (fontSize === undefined) {
+						if (typeof this.fontSize === 'function') {
+							fontSize = this.fontSize(word);
+						} else {
+							fontSize = this.fontSize;
+						}
+					}
+					if (fontFamily === undefined) {
+						if (typeof this.fontFamily === 'function') {
+							fontFamily = this.fontFamily(word);
+						} else {
+							fontFamily = this.fontFamily;
+						}
+					}
+					if (fontStyle === undefined) {
+						if (typeof this.fontStyle === 'function') {
+							fontStyle = this.fontStyle(word);
+						} else {
+							fontStyle = this.fontStyle;
+						}
+					}
+					if (fontWeight === undefined) {
+						if (typeof this.fontWeight === 'function') {
+							fontWeight = this.fontWeight(word);
+						} else {
+							fontWeight = this.fontWeight;
+						}
+					}
+					if (rotate === undefined) {
+						if (typeof this.rotate === 'function') {
+							rotate = this.rotate(word);
+						} else {
+							rotate = this.rotate;
+						}
+					}
+					return {text, fontSize, fontFamily, fontStyle, fontWeight, rotate};
+				});
+			},
 		},
 
 		watch: {
-			data: {
-				handler(data) {
-					this.chart.data = data;
-					this.chart.update();
+			normalizedWords: {
+				handler(words) {
+					let id = this.drawId = {};
+					setTimeout(() => {
+						this.computePlacedWords(id, words);
+					}, 1);
 				},
-				deep: true,
+				immediate: true,
 			},
+		},
 
-			options: {
-				handler(options) {
-					this.chart.options = options;
-					this.chart.update();
-				},
-				deep: true,
+		methods: {
+			async computePlacedWords(id, words) {
+				if (this.drawId !== id) {
+					return;
+				}
+				let returns = [];
+				for (let {text, fontSize, fontFamily, fontStyle, fontWeight, rotate} of words) {
+					returns.push({
+						text,
+						style: {
+							left: `${Math.random() * 500}px`,
+							top: `${Math.random() * 500}px`,
+							fontSize: `${fontSize}px`,
+							fontFamily,
+							fontStyle,
+							fontWeight,
+						},
+					});
+					await timeout(1);
+					if (this.drawId !== id) {
+						return;
+					}
+				}
+				this.placedWords = returns;
 			},
 		},
 	});
--- demo/demo.js
+++ demo/demo.js
@@ -69,7 +69,7 @@ this.VueWordCloud = function() {
 	  }
 	  if (i >= n) {
 		cloud.stop();
-		drawNow(tags);
+		//drawNow(tags);
 		//event.call("end", cloud, tags, bounds);
 	  }
 	}
@@ -472,3 +472,29 @@ function drawNow(words) {
 	  .text(function(d) { return d.text; });
 	drawNow2(words);
 }
+
+
+new Vue({
+	el: '#demo',
+
+	data() {
+		let fontFamilyValues = [
+			'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Georgia',
+			'Impact', 'Lucida Console', 'Tahoma', 'Times New Roman', 'Verdana',
+			'serif', 'sans-serif', 'monospace',
+		];
+
+		return {
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			fontFamily: fontFamilyValues[0],
+			fontFamilyValues,
+		};
+	},
+
+	computed: {
+		words() {
+			return this.text.split(/[^0-9a-zA-Z]+/g).map(text => [text, 12 + Math.random() * 24]);
+		},
+	},
+
+});
