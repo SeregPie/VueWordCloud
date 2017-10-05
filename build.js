5a6dbd0a4ee1c8d4b031c61a5b88d656116eafa9
seregpie@gmail.com <seregpie@gmail.com>
2017-10-05 23:20:06 +0200

    17.10.5

--- build.js
+++ build.js
@@ -2,56 +2,81 @@
 
 const fs = require('fs');
 const path = require('path');
-const babel = require('babel-core');
+const rollup = require('rollup');
+const babel = require('rollup-plugin-babel');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const minify = require('rollup-plugin-babel-minify');
 
-let transformFile;
-
-let build = function() {
-	try {
-		for (let [output, options] of Object.entries({
-			'VueWordCloud.min.js': {
-				presets: ['minify'],
-			},
-			/*'VueWordCloud.es2015.min.js': {
-				presets: [
-					['env', {
-						modules: false,
-						targets: {
-							'edge': 13,
-							'firefox': 49,
-							'ie': 11,
-							'ios': 9,
-							'safari': 9,
-						},
-						debug: true,
-					}],
-					'minify',
-				],
-				"plugins": ["transform-runtime"]
-			},
-			*/
-		})) {
-			let code = transformFile(path.join(__dirname, 'VueWordCloud.js'), options);
-			code = code.replace('\'./workers/boundWord.js\'', () => {
-				let code = transformFile(path.join(__dirname, './workers/boundWord.js'), options);
-				return JSON.stringify(code);
+let build = async function(plugins, output) {
+	let id = './workers/boundWord.js';
+	let workers = {
+		[id]: await (async () => {
+			let bundle = await rollup.rollup(Object.assign({
+				input: path.join(__dirname, 'src', id),
+				plugins,
+			}));
+			let {code} = await bundle.generate({
+				format: 'iife',
 			});
-			fs.writeFileSync(path.join(__dirname, output), code);
-		}
-		console.log('build complete');
-	} catch (error) {
-		console.error('build failed', error);
+			return code;
+		})(),
+	};
+
+	let bundle = await rollup.rollup({
+		input: path.join(__dirname, 'src', 'VueWordCloud.js'),
+		plugins,
+		external: ['vue'],
+	});
+	let {code} = await bundle.generate({
+		format: 'umd',
+		name: 'VueWordCloud',
+		globals: {
+			'vue': 'Vue',
+		},
+	});
+	for (let [workerId, workerCode] of Object.entries(workers)) {
+		code = code.replace('\''+workerId+'\'', JSON.stringify(workerCode));
 	}
+
+	fs.writeFileSync(output, code);
 };
 
-let watchedFiles = new Set();
-transformFile = function(file, options) {
-	if (!watchedFiles.has(file)) {
-		watchedFiles.add(file);
-		fs.watchFile(file, build);
-	}
-	let {code} = babel.transformFileSync(file, options);
-	return code;
-};
+build(
+	[
+		minify({
+			comments: false,
+		}),
+	],
+	path.join(__dirname, 'VueWordCloud.min.js'),
+);
 
-build();
+build(
+	[
+		babel({
+			exclude: 'node_modules/**',
+			presets: [
+				['env', {
+					modules: false,
+					targets: {
+						'browsers': ['last 2 versions'],
+						'edge': 13,
+						'firefox': 49,
+						'ie': 11,
+						'safari': 9,
+					},
+				}],
+			],
+			plugins: ['transform-runtime'],
+			runtimeHelpers: true,
+		}),
+		nodeResolve(),
+		commonjs({
+			include: 'node_modules/**',
+		}),
+		minify({
+			comments: false,
+		}),
+	],
+	path.join(__dirname, 'VueWordCloud.es2015.min.js'),
+);
