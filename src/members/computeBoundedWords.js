9e94ba94182e17796a76d6c56fa15b78edefa3cd
seregpie@gmail.com <seregpie@gmail.com>
2017-10-15 11:50:19 +0200

    add padding to minimize text overlapping

--- src/members/computeBoundedWords.js
+++ src/members/computeBoundedWords.js
@@ -4,47 +4,17 @@ import D2_rectAfterRotation from '../helpers/D2/rectAfterRotation';
 
 import boundWordWorkerContent from 'objectURL!../workers/boundWord.js';
 
-let getTextRect = async function(text, fontFamily, fontSize, fontStyle, fontVariant, fontWeight, rotation) {
-	rotation = Math_turnToRad(rotation);
-	let font = [fontStyle, fontVariant, fontWeight, `${fontSize}px`, fontFamily].join(' ');
-	try {
-		await document.fonts.load(font,  text);
-	} catch (error) {
-		// continue regardless of error
-	}
-	let ctx = document.createElement('canvas').getContext('2d');
-	ctx.font = font;
-	let textWidth = ctx.measureText(text).width;
-	let textHeight = fontSize;
-	let [rectWidth, rectHeight] = D2_rectAfterRotation(textWidth, textHeight, rotation);
-	let rectData = new Uint8Array(rectWidth * rectHeight);
-	if (rectData.length > 0) {
-		let ctx = document.createElement('canvas').getContext('2d');
-		ctx.canvas.width = rectWidth;
-		ctx.canvas.height = rectHeight;
-		ctx.translate(rectWidth / 2, rectHeight / 2);
-		ctx.rotate(rotation);
-		ctx.font = font;
-		ctx.textAlign = 'center';
-		ctx.textBaseline = 'middle';
-		ctx.fillText(text, 0, 0);
-		let imageData = ctx.getImageData(0, 0, rectWidth, rectHeight).data;
-		for (let i = 0, ii = rectData.length; i < ii; ++i) {
-			rectData[i] = imageData[i * 4 + 3];
-		}
-	}
-	return {textWidth, textHeight, rectWidth, rectHeight, rectData};
-};
-
 export default async function(context) {
 	let containerWidth = this.containerWidth;
 	let containerHeight = this.containerHeight;
 	if (containerWidth <= 0 || containerHeight <= 0) {
 		return [];
 	}
-	let words = this.normalizedWords;
-	await context.delayIfNotInterrupted();
 	let containerAspect = containerWidth / containerHeight;
+	let words = this.normalizedWords;
+
+	await context.delayIfNotInterrupted();
+
 	let boundedWords = [];
 	let boundWordWorker = new Worker(boundWordWorkerContent);
 	try {
@@ -55,12 +25,82 @@ export default async function(context) {
 		for (let word of words) {
 			context.throwIfInterrupted();
 			try {
-				let {text, weight, rotation, fontFamily, fontStyle, fontVariant, fontWeight, color} = word;
+				let {
+					text,
+					weight,
+					rotation,
+					fontFamily,
+					fontStyle,
+					fontVariant,
+					fontWeight,
+					color,
+				} = word;
+
 				let fontSize = weight * 4;
-				let {textWidth, textHeight, rectWidth, rectHeight, rectData} = await getTextRect(text, fontFamily, fontSize, fontStyle, fontVariant, fontWeight, rotation);
-				boundWordWorker.postMessage({rectWidth, rectHeight, rectData});
-				let {rectLeft, rectTop} = await Worker_getMessage(boundWordWorker);
-				boundedWords.push({text, rotation, fontFamily, fontSize, fontStyle, fontVariant, fontWeight, rectLeft, rectTop, rectWidth, rectHeight, textWidth, textHeight, color});
+				let rotationRad = Math_turnToRad(rotation);
+
+				let font = [fontStyle, fontVariant, fontWeight, `${fontSize}px`, fontFamily].join(' ');
+				try {
+					await document.fonts.load(font,  text);
+				} catch (error) {
+					// continue regardless of error
+				}
+
+				let ctx = document.createElement('canvas').getContext('2d');
+				ctx.font = font;
+				let innerTextWidth = ctx.measureText(text).width;
+				let innerTextHeight = fontSize;
+				let outerTextPadding = Math.max(ctx.measureText('m').width, fontSize);
+				let outerTextWidth = innerTextWidth + 2 * outerTextPadding;
+				let outerTextHeight = innerTextHeight + 2 * outerTextPadding;
+				let [innerRectWidth, innerRectHeight] = D2_rectAfterRotation(innerTextWidth, innerTextHeight, rotationRad);
+				let [outerRectWidth, outerRectHeight] = D2_rectAfterRotation(outerTextWidth, outerTextHeight, rotationRad);
+				let outerRectData = new Uint8Array(outerRectWidth * outerRectHeight);
+				if (outerRectData.length > 0) {
+					let ctx = document.createElement('canvas').getContext('2d');
+					ctx.canvas.width = outerRectWidth;
+					ctx.canvas.height = outerRectHeight;
+					ctx.translate(outerRectWidth / 2, outerRectHeight / 2);
+					ctx.rotate(rotationRad);
+					ctx.font = font;
+					ctx.textAlign = 'center';
+					ctx.textBaseline = 'middle';
+					ctx.fillText(text, 0, 0);
+					let imageData = ctx.getImageData(0, 0, outerRectWidth, outerRectHeight).data;
+					for (let i = 0, ii = outerRectData.length; i < ii; ++i) {
+						outerRectData[i] = imageData[i * 4 + 3];
+					}
+				}
+
+				boundWordWorker.postMessage({
+					rectWidth: outerRectWidth,
+					rectHeight: outerRectHeight,
+					rectData: outerRectData,
+				});
+				let {
+					rectLeft: outerRectLeft,
+					rectTop: outerRectTop,
+				} = await Worker_getMessage(boundWordWorker);
+
+				let innerRectLeft = outerRectLeft + (outerRectWidth - innerRectWidth) / 2;
+				let innerRectTop = outerRectTop + (outerRectHeight - innerRectHeight) / 2;
+
+				boundedWords.push({
+					text,
+					rotation,
+					fontFamily,
+					fontSize,
+					fontStyle,
+					fontVariant,
+					fontWeight,
+					rectLeft: innerRectLeft,
+					rectTop: innerRectTop,
+					rectWidth: innerRectWidth,
+					rectHeight: innerRectHeight,
+					textWidth: innerTextWidth,
+					textHeight: innerTextHeight,
+					color,
+				});
 			} catch (error) {
 				// console.log(error);
 				// continue regardless of error
