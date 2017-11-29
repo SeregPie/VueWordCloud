e08a564e60b30ff656b727891fa907486fee9c16
seregpie@gmail.com <seregpie@gmail.com>
2017-11-29 19:40:23 +0100

    avoid unnecessary placeWordPixels calls

--- src/members/getBoundedWords.js
+++ src/members/getBoundedWords.js
@@ -13,69 +13,25 @@ export default function(words, containerWidth, containerHeight, fontSizeRatio) {
 		let wordFontSizes = getWordFontSizes(words, fontSizeRatio);
 		words = Array_sortBy(words, ({key}) => -wordFontSizes[key]);
 
-		let {
-			key,
-			text,
-			rotation,
-			fontFamily,
-			fontStyle,
-			fontVariant,
-			fontWeight,
-		} = words.shift();
-		let fontSize = 4 * wordFontSizes[key];
-		let rotationRad = Math_turnToRad(rotation);
-		let ref = getWordCanvasMeasures(
-			text,
-			fontStyle,
-			fontVariant,
-			fontWeight,
-			fontSize,
-			fontFamily,
-			rotationRad,
-		);
-		let [textWidth, textHeight] = ref[0];
-		let [rectWidth, rectHeight] = ref[1];
-		let [image, imageWidth, imageHeight] = ref[2];
-
-		let totalImageResolution = Math.pow(2, 22);
-		let totalImageWidth = Math.floor(Math.sqrt(containerAspect * totalImageResolution));
-		let totalImageHeight = Math.floor(totalImageResolution / totalImageWidth);
-		//totalImageWidth = Math_ceilPowerOfTwo(totalImageWidth);
-		//totalImageHeight = Math_ceilPowerOfTwo(totalImageHeight);
-		let totalImage = new Uint8Array(totalImageWidth * totalImageHeight);
-
-		let imageLeft = Math.floor((totalImageWidth - imageWidth) / 2);
-		let imageTop = Math.floor((totalImageHeight - imageHeight) / 2);
-		placeWordPixels(
-			[totalImage, totalImageWidth, totalImageHeight],
-			[image, imageLeft, imageTop, imageWidth, imageHeight],
-		);
-		let rectLeft = imageLeft + (imageWidth - rectWidth) / 2;
-		let rectTop = imageTop + (imageHeight - rectHeight) / 2;
-
-		returns.push({
-			key,
-			fontSize,
-			textWidth,
-			textHeight,
-			rectLeft,
-			rectTop,
-			rectWidth,
-			rectHeight,
-		});
-
-		words.forEach(({
-			key,
-			text,
-			rotation,
-			fontFamily,
-			fontStyle,
-			fontVariant,
-			fontWeight,
-		}) => {
+		// avoid buble bug
+		(() => {
+			let {
+				key,
+				text,
+				rotation,
+				fontFamily,
+				fontStyle,
+				fontVariant,
+				fontWeight,
+			} = words.shift();
 			let fontSize = 4 * wordFontSizes[key];
 			let rotationRad = Math_turnToRad(rotation);
-			let ref = getWordCanvasMeasures(
+
+			let [
+				[textWidth, textHeight],
+				[rectWidth, rectHeight],
+				[image, imageWidth, imageHeight],
+			] = getWordCanvasMeasures(
 				text,
 				fontStyle,
 				fontVariant,
@@ -84,19 +40,16 @@ export default function(words, containerWidth, containerHeight, fontSizeRatio) {
 				fontFamily,
 				rotationRad,
 			);
-			let [textWidth, textHeight] = ref[0];
-			let [rectWidth, rectHeight] = ref[1];
-			let [image, imageWidth, imageHeight] = ref[2];
 
-			let [imageLeft, imageTop] = getWordImagePosition(
-				[totalImage, totalImageWidth, totalImageHeight],
-				[image, imageWidth, imageHeight],
-			);
+			let totalImageResolution = Math.pow(2, 22);
+			let totalImageWidth = Math.floor(Math.sqrt(containerAspect * totalImageResolution));
+			let totalImageHeight = Math.floor(totalImageResolution / totalImageWidth);
+			//totalImageWidth = Math_ceilPowerOfTwo(totalImageWidth);
+			//totalImageHeight = Math_ceilPowerOfTwo(totalImageHeight);
+			let totalImage = new Uint8Array(totalImageWidth * totalImageHeight);
 
-			placeWordPixels(
-				[totalImage, totalImageWidth, totalImageHeight],
-				[image, imageLeft, imageTop, imageWidth, imageHeight],
-			);
+			let imageLeft = Math.floor((totalImageWidth - imageWidth) / 2);
+			let imageTop = Math.floor((totalImageHeight - imageHeight) / 2);
 			let rectLeft = imageLeft + (imageWidth - rectWidth) / 2;
 			let rectTop = imageTop + (imageHeight - rectHeight) / 2;
 
@@ -110,7 +63,57 @@ export default function(words, containerWidth, containerHeight, fontSizeRatio) {
 				rectWidth,
 				rectHeight,
 			});
-		});
+
+			words.forEach(({
+				key,
+				text,
+				rotation,
+				fontFamily,
+				fontStyle,
+				fontVariant,
+				fontWeight,
+			}) => {
+				placeWordPixels(
+					[totalImage, totalImageWidth, totalImageHeight],
+					[image, imageLeft, imageTop, imageWidth, imageHeight],
+				);
+
+				fontSize = 4 * wordFontSizes[key];
+				rotationRad = Math_turnToRad(rotation);
+
+				([
+					[textWidth, textHeight],
+					[rectWidth, rectHeight],
+					[image, imageWidth, imageHeight],
+				] = getWordCanvasMeasures(
+					text,
+					fontStyle,
+					fontVariant,
+					fontWeight,
+					fontSize,
+					fontFamily,
+					rotationRad,
+				));
+
+				([imageLeft, imageTop] = getWordImagePosition(
+					[totalImage, totalImageWidth, totalImageHeight],
+					[image, imageWidth, imageHeight],
+				));
+				rectLeft = imageLeft + (imageWidth - rectWidth) / 2;
+				rectTop = imageTop + (imageHeight - rectHeight) / 2;
+
+				returns.push({
+					key,
+					fontSize,
+					textWidth,
+					textHeight,
+					rectLeft,
+					rectTop,
+					rectWidth,
+					rectHeight,
+				});
+			});
+		})();
 	}
 	return returns;
 }
