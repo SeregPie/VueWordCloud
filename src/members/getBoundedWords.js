949d4fc0ddd7d6110254b218a24638c6a3897833
seregpie@gmail.com <seregpie@gmail.com>
2017-12-12 07:23:27 +0100

    +

--- src/members/getBoundedWords.js
+++ src/members/getBoundedWords.js
@@ -16,140 +16,149 @@ export default function(words, containerWidth, containerHeight, fontSizeRatio) {
 		if (words.length > 0) {
 			let containerAspect = containerWidth / containerHeight;
 			let wordFontSizes = getWordFontSizes(words, fontSizeRatio);
-			words = words.map(({
-				key,
-				text,
-				rotation,
-				fontFamily,
-				fontStyle,
-				fontVariant,
-				fontWeight,
-			}) => {
-				let fontSize = Math.pow(aaaa - 1, 2) * wordFontSizes[key] + 1;
-				let [
-					textWidth,
-					textHeight,
-					rectWidth,
-					rectHeight,
-					safeRectWidth,
-					safeRectHeight,
-				] = getWordElementMeasures(
-					text,
-					fontStyle,
-					fontVariant,
-					fontWeight,
-					fontSize,
-					fontFamily,
-					rotation,
-				);
-				let imageWidth = Math_ceilPowerOfTwo(safeRectWidth);
-				let imageHeight = Math_ceilPowerOfTwo(safeRectHeight);
-				let image = getWordImage(
-					text,
-					fontStyle,
-					fontVariant,
-					fontWeight,
-					fontSize,
-					fontFamily,
-					imageWidth,
-					imageHeight,
-					rotation,
-				);
-				let cccc = Math.log2(Math.min(imageWidth, imageHeight) / Math.pow(aaaa, 2)); //renameMe
-				return {
+			return Promise
+				.all(words.map(({
 					key,
-					fontSize,
-					textWidth,
-					textHeight,
-					rectWidth,
-					rectHeight,
-					image,
-					imageWidth,
-					imageHeight,
-					cccc,
-				};
-			});
-			words = Array_sortBy(words, ({cccc}) => -cccc);
-			let totalImageLayers;
-			words.forEach(({
-				key,
-				fontSize,
-				textWidth,
-				textHeight,
-				rectWidth,
-				rectHeight,
-				image,
-				imageWidth,
-				imageHeight,
-				cccc,
-			}) => {
-				let imageLayers = [[image, imageWidth, imageHeight]];
-				for (let i = 1; i < cccc; ++i) {
-					([image, imageWidth, imageHeight] = getReducedWordImage(image, imageWidth, imageHeight, 2));
-					imageLayers.push([image, imageWidth, imageHeight]);
-				}
-				if (totalImageLayers === undefined) {
-					let resolution = Math.pow(2, 16); // fixMe (2^32 is max)
-					let totalImageWidth = Math.floor(Math.sqrt(containerAspect * resolution));
-					let totalImageHeight = Math.floor(resolution / totalImageWidth);
-					let totalImage = new Uint8Array(totalImageWidth * totalImageHeight);
-					totalImageLayers = [[totalImage, totalImageWidth, totalImageHeight]];
-					for (let i = 1; i < imageLayers.length; ++i) {
-						totalImageWidth *= 2;
-						totalImageHeight *= 2;
-						totalImage = new Uint8Array(totalImageWidth * totalImageHeight);
-						totalImageLayers.unshift([totalImage, totalImageWidth, totalImageHeight]);
-					}
-				}
-				let [totalImage, totalImageWidth, totalImageHeight] = totalImageLayers[imageLayers.length - 1];
-				let [imageLeft, imageTop] = getWordImagePosition(
-					totalImage,
-					totalImageWidth,
-					totalImageHeight,
-					image,
-					imageWidth,
-					imageHeight,
-				);
-				placeWordPixels(
-					totalImage,
-					totalImageWidth,
-					totalImageHeight,
-					imageLeft,
-					imageTop,
-					image,
-					imageWidth,
-					imageHeight,
-				);
-				for (let i = imageLayers.length - 1; i-- > 0;) {
-					imageLeft *= 2;
-					imageTop *= 2;
-					[totalImage, totalImageWidth, totalImageHeight] = totalImageLayers[i];
-					[image, imageWidth, imageHeight] = imageLayers[i];
-					placeWordPixels(
-						totalImage,
-						totalImageWidth,
-						totalImageHeight,
-						imageLeft,
-						imageTop,
+					text,
+					rotation,
+					fontFamily,
+					fontStyle,
+					fontVariant,
+					fontWeight,
+				}) =>
+					Promise
+						.resolve()
+						.then(() => {
+							let fontSize = Math.pow(aaaa - 1, 2) * wordFontSizes[key] + 1;
+							let [
+								textWidth,
+								textHeight,
+								rectWidth,
+								rectHeight,
+								safeRectWidth,
+								safeRectHeight,
+							] = getWordElementMeasures(
+								text,
+								fontStyle,
+								fontVariant,
+								fontWeight,
+								fontSize,
+								fontFamily,
+								rotation,
+							);
+							let imageWidth = Math_ceilPowerOfTwo(safeRectWidth);
+							let imageHeight = Math_ceilPowerOfTwo(safeRectHeight);
+							let image = getWordImage(
+								text,
+								fontStyle,
+								fontVariant,
+								fontWeight,
+								fontSize,
+								fontFamily,
+								imageWidth,
+								imageHeight,
+								rotation,
+							);
+							let cccc = Math.log2(Math.min(imageWidth, imageHeight) / Math.pow(aaaa, 2)); //renameMe
+							return {
+								key,
+								fontSize,
+								textWidth,
+								textHeight,
+								rectWidth,
+								rectHeight,
+								image,
+								imageWidth,
+								imageHeight,
+								cccc,
+							};
+						})
+				))
+				.then(words => Array_sortBy(words, ({cccc}) => -cccc))
+				.then(words => {
+					let returns = [];
+					let totalImageLayers;
+					words.forEach(({
+						key,
+						fontSize,
+						textWidth,
+						textHeight,
+						rectWidth,
+						rectHeight,
 						image,
 						imageWidth,
 						imageHeight,
-					);
-				}
-				let rectLeft = imageLeft + (imageWidth - rectWidth) / 2;
-				let rectTop = imageTop + (imageHeight - rectHeight) / 2;
-				returns.push({
-					key,
-					fontSize,
-					textWidth,
-					textHeight,
-					rectLeft,
-					rectTop,
-					rectWidth,
-					rectHeight,
+						cccc,
+					}) => {
+						let imageLayers = [[image, imageWidth, imageHeight]];
+						for (let i = 1; i < cccc; ++i) {
+							([image, imageWidth, imageHeight] = getReducedWordImage(image, imageWidth, imageHeight, 2));
+							imageLayers.push([image, imageWidth, imageHeight]);
+						}
+						if (totalImageLayers === undefined) {
+							let resolution = Math.pow(2, 16); // fixMe (2^32 is max)
+							let totalImageWidth = Math.floor(Math.sqrt(containerAspect * resolution));
+							let totalImageHeight = Math.floor(resolution / totalImageWidth);
+							let totalImage = new Uint8Array(totalImageWidth * totalImageHeight);
+							totalImageLayers = [[totalImage, totalImageWidth, totalImageHeight]];
+							for (let i = 1; i < imageLayers.length; ++i) {
+								totalImageWidth *= 2;
+								totalImageHeight *= 2;
+								totalImage = new Uint8Array(totalImageWidth * totalImageHeight);
+								totalImageLayers.unshift([totalImage, totalImageWidth, totalImageHeight]);
+							}
+						}
+						let [totalImage, totalImageWidth, totalImageHeight] = totalImageLayers[imageLayers.length - 1];
+						let [imageLeft, imageTop] = getWordImagePosition(
+							totalImage,
+							totalImageWidth,
+							totalImageHeight,
+							image,
+							imageWidth,
+							imageHeight,
+						);
+						placeWordPixels(
+							totalImage,
+							totalImageWidth,
+							totalImageHeight,
+							imageLeft,
+							imageTop,
+							image,
+							imageWidth,
+							imageHeight,
+						);
+						for (let i = imageLayers.length - 1; i-- > 0;) {
+							imageLeft *= 2;
+							imageTop *= 2;
+							[totalImage, totalImageWidth, totalImageHeight] = totalImageLayers[i];
+							[image, imageWidth, imageHeight] = imageLayers[i];
+							placeWordPixels(
+								totalImage,
+								totalImageWidth,
+								totalImageHeight,
+								imageLeft,
+								imageTop,
+								image,
+								imageWidth,
+								imageHeight,
+							);
+						}
+						let rectLeft = imageLeft + (imageWidth - rectWidth) / 2;
+						let rectTop = imageTop + (imageHeight - rectHeight) / 2;
+						returns.push({
+							key,
+							fontSize,
+							textWidth,
+							textHeight,
+							rectLeft,
+							rectTop,
+							rectWidth,
+							rectHeight,
+						});
+					});
+					return returns;
 				});
-			});
 		}
 	}
-	return returns;
+	return [];
 }
