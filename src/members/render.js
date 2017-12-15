1cce7e540f75811ff99ac1ea93e593a403e3f160
seregpie@gmail.com <seregpie@gmail.com>
2017-12-16 00:26:18 +0100

    add transitionDelay

--- src/members/render.js
+++ src/members/render.js
@@ -4,19 +4,65 @@ export default function(
 	words,
 	animationDuration,
 ) {
-	return (
-		createElement(
+	let transitionDuration = words.length > 0
+		? animationDuration / Math.min(4, words.length)
+		: 0;
+	let transitionDelay = words.length > 1
+		? (animationDuration - transitionDuration) / (words.length - 1)
+		: 0;
+	return createElement(
+		'div',
+		{
+			style: {
+				position: 'relative',
+				width: '100%',
+				height: '100%',
+				overflow: 'hidden',
+			},
+		},
+		[createElement(
 			'div',
 			{
 				style: {
-					position: 'relative',
-					width: '100%',
-					height: '100%',
-					overflow: 'hidden',
+					position: 'absolute',
+					top: '50%',
+					left: '50%',
+					transform: 'translate(-50%, -50%)',
 				},
 			},
-			[
-				createElement(
+			words.map(({
+				originalWord,
+				key,
+				text,
+				weight,
+				rotation,
+				fontFamily,
+				fontStyle,
+				fontVariant,
+				fontWeight,
+				color,
+				fontSize,
+				textWidth,
+				textHeight,
+				rectLeft,
+				rectTop,
+				rectWidth,
+				rectHeight,
+			}, index) => createElement(
+				'div',
+				{
+					key,
+					style: {
+						position: 'absolute',
+						top: `${rectTop + rectHeight / 2}px`,
+						left: `${rectLeft + rectWidth / 2}px`,
+						transform: `rotate(${rotation}turn)`,
+						color: color,
+						transition: ['all', `${transitionDuration}ms`, 'ease-in-out', `${transitionDelay * index}ms`].join(' '),
+						font: [fontStyle, fontVariant, fontWeight, `${fontSize}px/1`, fontFamily].join(' '),
+					},
+				},
+				[createElement(
 					'div',
 					{
 						style: {
@@ -24,11 +70,13 @@ export default function(
 							top: '50%',
 							left: '50%',
 							transform: 'translate(-50%, -50%)',
+							//width: `${textWidth}px`,
+							//height: `${textHeight}px`,
+							whiteSpace: 'nowrap',
 						},
 					},
-					words.map(({
+					[scopedSlots.default({
 						originalWord,
-						key,
 						text,
 						weight,
 						rotation,
@@ -38,63 +86,10 @@ export default function(
 						fontWeight,
 						color,
 						fontSize,
-						textWidth,
-						textHeight,
-						rectLeft,
-						rectTop,
-						rectWidth,
-						rectHeight,
-					}) =>
-						createElement(
-							'div',
-							{
-								key,
-								style: {
-									position: 'absolute',
-									top: `${rectTop + rectHeight / 2}px`,
-									left: `${rectLeft + rectWidth / 2}px`,
-									transform: `rotate(${rotation}turn)`,
-									color: color,
-									transitionDuration: `${Math.round(animationDuration)}ms`,
-									transitionProperty: 'all',
-									font: [fontStyle, fontVariant, fontWeight, `${fontSize}px/1`, fontFamily].join(' '),
-								},
-							},
-							[
-								createElement(
-									'div',
-									{
-										style: {
-											position: 'absolute',
-											top: '50%',
-											left: '50%',
-											transform: 'translate(-50%, -50%)',
-											//width: `${textWidth}px`,
-											//height: `${textHeight}px`,
-											whiteSpace: 'nowrap',
-										},
-									},
-									[
-										scopedSlots.default({
-											originalWord,
-											text,
-											weight,
-											rotation,
-											fontFamily,
-											fontStyle,
-											fontVariant,
-											fontWeight,
-											color,
-											fontSize,
-										}),
-									],
-								),
-							],
-						)
-					),
-				),
-			],
-		)
+					})],
+				)],
+			)),
+		)],
 	);
 
 }
