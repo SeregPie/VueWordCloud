e147439d8b57ed67343d160c2eaf6ef2a45089d9
Zhivka Dimova <zhivka.dimova@myforest.net>
2023-01-20 19:04:35 +0100

    Updating the demo
    Signed-off-by: Zhivka Dimova <zhivka.dimova@myforest.net>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-export {default} from './VueWordCloud';
+import VueWordCloud from './VueWordCloud';
+
+{
+	let {window} = globalThis;
+	if (window) {
+		let {Vue} = window;
+		if (Vue) {
+			Vue.defineComponent(VueWordCloud.name, VueWordCloud);
+		}
+	}
+}
+
+export default VueWordCloud;
